535edaf37d2b80510e8eb81cd928c409d214a27e
randi3281 <randi3281@gmail.com>
2024-10-10 09:33:18 +0700

    Add data frame and plot in learn.r

--- learn.r
+++ learn.r
@@ -1,4 +1,6 @@
-# /home/anfiniti/Documents/anfir/learn.r
-
-# Print "Hello, World!" to the console
-print("Hello, World!")
+nama = c('budi', 'mey', 'refi', 'mega', 'jimmy')
+uang = c(20000, 21111, 2222, 44444, 22221)
+belajar = c(3,2,1,4,5)
+df = data.frame(nama, uang, belajar)
+df
+plot(uang,belajar)
